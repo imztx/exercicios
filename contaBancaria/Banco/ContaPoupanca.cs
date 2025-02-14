5cfd87e9e6ed5bbee81b64b450bfec8a7a263fb2
Luiz Felipe <145378321+imztx@users.noreply.github.com>
2025-02-14 16:44:09 -0300

    Add files via upload

---
+++ contaBancaria/Banco/ContaPoupanca.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco
+{
+    public class ContaPoupanca : Conta  //Herança
+    {
+        public override double CalcularValorTarifaManutencao() //Sobrescrita de método
+        {
+            return Saldo * 0.2;
+        }
+    }
+}
