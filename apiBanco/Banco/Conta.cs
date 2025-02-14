7818ac1c1a0b6400f6f8e4a6823b328e81f8863b
Luiz Felipe <145378321+imztx@users.noreply.github.com>
2025-02-14 16:41:01 -0300

    Add files via upload

---
+++ apiBanco/Banco/Conta.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco
+{
+    public class Conta
+    {
+        public int Numero { get; set; }
+        public double Saldo { get; set; }
+        public double Limite { get; set; }
+        public string Nome { get; set; }
+
+        public double Taxa { get; set; }
+
+        //public Conta(int numero, double saldo, string nome)
+        //{
+        //    Numero = numero;
+        //    Saldo = saldo;
+        //    Nome = nome;
+        //}
+
+        public virtual double CalcularValorTarifaManutencao()
+        {
+            var calculo = Saldo * 0.1;
+            return calculo;
+        }
+
+        public virtual bool Saca(double valor)
+        {
+            if (valor<= Saldo)
+            {
+                Saldo -= valor;
+                return true;
+                
+            }
+            return false;
+
+        }
+
+        public void Deposita(double valor)
+        {
+            Saldo += valor;
+        }
+
+        public void Transfere(Conta contaDestino, double valor)
+        {
+            //Conta destino = new Conta();
+            contaDestino.Saldo += valor;
+        }
+    }
+}
---
+++ apiBanco/Banco/ContaCorrente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco
+{
+    internal class ContaCorrente : Conta
+    {
+        public override bool Saca(double valor)
+        {
+            double calculo = Saldo - 0.20;
+            return base.Saca(valor);
+        }
+    }
+}
