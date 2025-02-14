f4b8cc826ea7dd52eb392ba344c91008f7b440e3
Luiz Felipe <145378321+imztx@users.noreply.github.com>
2025-02-14 16:47:16 -0300

    Add files via upload

---
+++ SistemaUniversitario/Funcionario.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaUniversitario
+{
+    internal class Funcionario : Pessoa
+    {
+        public string Cargo { get; set; }
+
+        public string Departamento { get; set; }
+
+        public override void ExibirInformacoes()
+        {
+            base.ExibirInformacoes();
+            Console.WriteLine($"Cargo: {Cargo}\nDepartamento: {Departamento}");
+        }
+
+        public override double CalculaSalario(double valor)
+        {
+            return base.CalculaSalario(valor - (valor * 0.3));
+        }
+    }
+}
